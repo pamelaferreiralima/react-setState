d2b9a32f39e9bf3c5bf58d9c9fe13ac59c9b4808
Pâmela <pamela.potenciatech@gmail.com>
2020-11-11 16:36:47 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
-// Crie dois states onde cada state guarde um number diferente;
-// Crie uma arrow function que some os dois números e salve no seu primeiro state;
-// Adicione o evento onClick, para executar a função;
-// Mostre no H1 o seu primeiro state;
-// Use o map para retornar o array de musicas na li;
 import React, { Component } from "react";
 import "./styles.css";
 
